b6d143cb066388fcdbfc926d00f163a8f7e1dbeb
Lashon81 <lashon_riley81@yahoo.com>
2025-08-22 19:42:25 -0400

    update changes to index.html

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 setProperty ("Title", "color", "orange");
+onEvent("spanishBtn", "click", function() {
+    setText("spanishBtn", Aquí está la traducción al español de la frase que solicitaste);
+});
