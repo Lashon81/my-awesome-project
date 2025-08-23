94dcb5f7c74b22048e37f350842853c9bc4f0c0a
Lashon81 <lashon_riley81@yahoo.com>
2025-08-22 22:04:39 -0400

    made changes to index.html and index.js

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-setProperty ("Title", "color", "orange");
-onEvent("spanishBtn", "click", function() {
-    setText("spanishBtn", Aquí está la traducción al español de la frase que solicitaste);
+setProperty("WaterAppHeader", "color", "red");
+console.log("hello");
+setProperty("WaterAppHeader", "background-color", "blue");
+
+onEvent("esButton", "click", function(){
+    console.log("clicked spanish.");
+setText("WaterAppHeader", "consejos de Conservacion del Aqua");
+setText("subHeader", "Para conservar el agua es importante que todos pongamos de nuestra parte. Haz clic en esta aplicación para ver ideas de cómo hacerlo.");
+});
+onEvent("enButton", "click", function(){
+    console.log("clicked english.");
+setText("WaterAppHeader", "Water Conservation Tips");
+setText("subHeader", " It's important that we all do our part to use less water. Click through this app for conservation ideas.");
 });
